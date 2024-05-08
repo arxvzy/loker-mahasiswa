f4f0556a76bb7c16cd334738b05bb1de3f5a4a9b
arxvzy <szaryabima@gmail.com>
2024-05-08 18:36:03 +0700

    add navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg navbar-dark"
+    style="background-color: rgb(45 62 80)"
+  >
+    <div class="container">
+      <a class="navbar-brand text-bold" href="/">LokerMahasiswa</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="/">Pasang Loker</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/loker">Cari Loker</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
