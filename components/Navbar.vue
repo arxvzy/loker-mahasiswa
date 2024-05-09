8b84c88e52b5279d0a3c8be356daf6db100f8faf
arxvzy <szaryabima@gmail.com>
2024-05-09 17:28:15 +0700

    redesign navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,31 +1,5 @@
 <template>
-  <nav
-    class="navbar navbar-expand-lg navbar-dark"
-    style="background-color: rgb(45 62 80)"
-  >
-    <div class="container">
-      <a class="navbar-brand text-bold" href="/">LokerMahasiswa</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" href="/">Pasang Loker</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/loker">Cari Loker</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <div class="navbar bg-[#2d3e50] text-white">
+    <a class="btn btn-ghost text-xl">Loker Mahasiswa</a>
+  </div>
 </template>
