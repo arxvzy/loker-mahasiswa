8a2545ff6c99049812e5d8504eaf09f06ea83d81
arxvzy <szaryabima@gmail.com>
2024-05-09 17:29:00 +0700

    create post loker form

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,91 +1,210 @@
-<template>
-  <form class="w-50 m-auto mt-5">
-    <h1 class="text-center">Pasang Loker</h1>
-    <div class="mb-3">
-      <label for="posisi" class="form-label">Posisi Pekerjaan</label>
-      <input type="text" class="form-control" id="posisi" />
-    </div>
-    <div class="mb-3">
-      <label for="nama" class="form-label">Nama Badan Usaha</label>
-      <input type="text" class="form-control" id="nama" />
-    </div>
-    <div class="mb-3">
-      <label for="deskripsiUsaha" class="form-label">Deskripsi Usaha</label>
-      <input type="text" class="form-control" id="deskripsiUsaha" />
-    </div>
-    <div class="mb-3">
-      <label for="pendidikan" class="form-label">Minimal Pendidikan</label>
-      <select
-        class="form-select"
-        id="pendidikan"
-        aria-label="Default select example"
-      >
-        <option selected disabled>Pilih Pendidikan</option>
-        <option value="1">SD</option>
-        <option value="2">SMP</option>
-        <option value="3">SMA/SMK</option>
-        <option value="4">S1</option>
-        <option value="5">Tidak Ada Minimal Pendidikan</option>
-      </select>
-    </div>
-    <div class="mb-3">
-      <label for="gender" class="form-label">Jenis Kelamin</label>
-      <select
-        class="form-select"
-        id="gender"
-        aria-label="Default select example"
-      >
-        <option selected disabled>Pilih Jenis Kelamin</option>
-        <option value="1">Pria</option>
-        <option value="2">Wanita</option>
-        <option value="3">Pria/Wanita</option>
-      </select>
-    </div>
-    <div class="mb-3">
-      <label for="waktu" class="form-label">Waktu Pekerjaan</label>
-      <select
-        class="form-select"
-        id="waktu"
-        aria-label="Default select example"
-      >
-        <option selected disabled>Pilih Jenis Waktu Kerja</option>
-        <option value="1">Full-Time</option>
-        <option value="2">Part-Time</option>
-      </select>
-    </div>
-    <div class="mb-3">
-      <label for="lokasi" class="form-label">Lokasi</label>
-      <input type="text" class="form-control" id="lokasi" />
-    </div>
-    <div class="mb-3">
-      <label for="daftar" class="form-label">Panduan Mendaftar</label>
-      <textarea class="form-control" id="daftar" rows="3"></textarea>
-    </div>
-    <div class="mb-3">
-      <label for="kategori" class="form-label">Kategori Wilayah</label>
-      <select
-        class="form-select"
-        id="kategori"
-        aria-label="Default select example"
-      >
-        <option selected disabled>Pilih Kategori</option>
-        <option value="1">Semarang</option>
-        <option value="2">Jogja</option>
-        <option value="3">Medan</option>
-        <option value="4">Se-Indonesia</option>
-      </select>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-</template>
-
 <script setup>
-useHead({
-  link: [
-    {
-      rel: "stylesheet",
-      href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css",
-    },
-  ],
-});
+const toast = useToast();
+const allPendidikan = [
+  "SD",
+  "SMP",
+  "SMA/SMK",
+  "D3",
+  "S1",
+  "Tidak Ada Minimal Pendidikan",
+];
+const allGender = ["Pria", "Wanita", "Pria/Wanita"];
+const allWaktu = ["Full-Time", "Part-Time"];
+const allKategori = ["Semarang", "Jogja", "Medan", "Semua Daerah"];
+
+const posisi = ref("");
+const perusahaan = ref("");
+const deskripsiPerusahaan = ref("");
+const pendidikan = ref("");
+const gender = ref("");
+const waktu = ref("");
+const alamat = ref("");
+const daftar = ref("");
+const kategori = ref("");
+const jobdesk = ref([]);
+const syarat = ref([]);
+
+let jobdeskCount = ref(1);
+let syaratCount = ref(1);
+const postLoker = async () => {
+  const body = {
+    posisi: posisi.value,
+    nama_perusahaan: perusahaan.value,
+    deskripsi_perusahaan: deskripsiPerusahaan.value,
+    pendidikan: pendidikan.value,
+    gender: gender.value,
+    durasi: waktu.value,
+    alamat: alamat.value,
+    daftar: daftar.value,
+    kategori: kategori.value,
+    jobdesk: [],
+    syarat: [],
+  };
+  jobdesk.value.forEach((el) => {
+    body.jobdesk.push(el);
+  });
+  syarat.value.forEach((el) => {
+    body.syarat.push(el);
+  });
+  try {
+    const response = await fetch("/api/loker/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((json) => {
+        return json.json();
+      })
+      .then((res) => {
+        if (res.success) {
+          toast.add({ title: "Loker Berhasil Diposting!", color: "primary" });
+        } else {
+          toast.add({ title: "Loker Gagal Diposting!", color: "red" });
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
+
+<template>
+  <div class="text-black mt-3 w-1/2 mx-auto">
+    <p>{{ jobdesk.value }}</p>
+    <h1 class="text-center text-xl">Pasang Loker</h1>
+    <form class="m-auto mt-5">
+      <div class="mb-3">
+        <label>Posisi Pekerjaan</label>
+        <UInput size="md" color="blue" variant="outline" v-model="posisi" />
+      </div>
+      <div class="mb-3">
+        <label>Nama Perusahaan</label>
+        <UInput size="md" color="blue" variant="outline" v-model="perusahaan" />
+      </div>
+      <div class="mb-3">
+        <label>Deskripsi Perusahaan</label>
+        <UInput
+          size="md"
+          color="blue"
+          variant="outline"
+          v-model="deskripsiPerusahaan"
+        />
+      </div>
+      <div class="mb-3">
+        <label>Minimal Pendidikan</label>
+        <USelect
+          v-model="pendidikan"
+          color="blue"
+          variant="outline"
+          :options="allPendidikan"
+          placeholder="Pilih Minimal Pendidikan"
+          size="md"
+        />
+      </div>
+      <div class="mb-3">
+        <label>Jenis Kelamin</label>
+        <USelect
+          v-model="gender"
+          color="blue"
+          variant="outline"
+          :options="allGender"
+          placeholder="Pilih Jenis Kelamin"
+          size="md"
+        />
+      </div>
+      <div class="mb-3">
+        <label>Waktu Kerja</label>
+        <USelect
+          v-model="waktu"
+          color="blue"
+          variant="outline"
+          :options="allWaktu"
+          placeholder="Pilih Waktu kerja"
+          size="md"
+        />
+      </div>
+      <div class="mb-3">
+        <label>Alamat</label>
+        <UInput size="md" color="blue" variant="outline" v-model="alamat" />
+      </div>
+      <div class="mb-3">
+        <label>Panduan Mendaftar</label>
+        <UTextarea color="blue" variant="outline" v-model="daftar" />
+      </div>
+      <div class="mb-3">
+        <label>Kategori Wilayah</label>
+        <USelect
+          v-model="kategori"
+          color="blue"
+          variant="outline"
+          :options="allKategori"
+          placeholder="Pilih Kategori Wilayah"
+          size="md"
+        />
+      </div>
+      <label class="mb-3">Jobdesk</label>
+      <div v-for="index in jobdeskCount" class="flex justify-start pb-3">
+        <UInput
+          size="md"
+          color="blue"
+          variant="outline"
+          v-model="jobdesk[index]"
+          class="w-full mr-2"
+        />
+        <button
+          @click.prevent="jobdeskCount++"
+          class="btn btn-sm btn-square btn-info my-auto"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            stroke="white"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"
+            />
+          </svg>
+        </button>
+      </div>
+      <label class="mb-3">Syarat Melamar Pekerjaan</label>
+      <div v-for="index in syaratCount" class="flex justify-start pb-3">
+        <UInput
+          size="md"
+          color="blue"
+          variant="outline"
+          v-model="syarat[index]"
+          class="w-full mr-2"
+        />
+        <button
+          @click.prevent="syaratCount++"
+          class="btn btn-sm btn-square btn-info my-auto"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            stroke="white"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div class="mt-3 flex justify-center">
+        <UButton size="lg" @click.prevent="postLoker">POST</UButton>
+      </div>
+    </form>
+  </div>
+</template>
