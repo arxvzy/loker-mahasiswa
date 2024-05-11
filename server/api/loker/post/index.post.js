1ad95e7853a0d2f9b243c63b8df5838d10ab5105
arxvzy <szaryabima@gmail.com>
2024-05-11 16:47:36 +0700

    changed database provider from mysql to supabase's postgres

--- server/api/loker/post/index.post.js
+++ server/api/loker/post/index.post.js
@@ -1,46 +1,28 @@
-import { prisma } from "~/server/prisma";
+import { prisma } from "~/server/prisma.js";
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
+  const raw = await readBody(event);
+  const body = JSON.parse(raw);
+  const data = {
+    jobdesk: {
+      create: body.jobdesk,
+    },
+    syarat: {
+      create: body.syarat,
+    },
+    ...body.general,
+  };
   try {
-    const lokerdb = await prisma.ketentuan_loker.create({
-      data: {
-        posisi: body.posisi,
-        nama_perusahaan: body.nama_perusahaan,
-        deskripsi_perusahaan: body.deskripsi_perusahaan,
-        pendidikan: body.pendidikan,
-        gender: body.gender,
-        durasi: body.durasi,
-        alamat: body.alamat,
-        daftar: body.daftar,
-        kategori: body.kategori,
+    await prisma.general.create({
+      data: data,
+      include: {
+        jobdesk: true,
+        syarat: true,
       },
     });
-    if (lokerdb.id) {
-      const jobdesk = [];
-      body.jobdesk.forEach((el) => {
-        jobdesk.push({ jobdesk: el, loker_id: lokerdb.id });
-      });
-      const jobdeskdb = await prisma.deskripsi_pekerjaan.createMany({
-        data: jobdesk,
-      });
-      if (jobdeskdb.count) {
-        const syarat = [];
-        body.syarat.forEach((el) => {
-          syarat.push({ syarat: el, loker_id: lokerdb.id });
-        });
-        const syaratdb = await prisma.syarat_pekerjaan.createMany({
-          data: syarat,
-        });
-        if (syaratdb.count) {
-          return await {
-            success: true,
-            dataLoker: lokerdb,
-            jobdeskCount: jobdeskdb,
-            syaratCount: syaratdb,
-          };
-        } else return { success: false, message: "Gagal Create Table Syarat" };
-      } else return { success: false, message: "Gagal Create Table Jobdesk" };
-    } else return { success: false, message: "Gagal Create Table Loker" };
+    return {
+      success: true,
+      message: "Loker Berhasil Di Posting",
+    };
   } catch (err) {
     throw err;
   }
