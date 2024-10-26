3cb3476d9709f79152fb6f26ccd01ce0bf97abb3
Arxvzy <szaryabima@gmail.com>
2024-10-26 15:45:39 +0700

    refactor: separate instagram account list for modularity

---
+++ resources/views/components/list-ig.blade.php
@@ -0,0 +1,36 @@
+<div class="mt-2 hover:underline"><a href="https://instagram.com/lokermahasiswa_id"
+        class="flex flex-row gap-2 items-center text-center" target="_blank">
+        <img src="/images/instagram.png" alt="">@lokermahasiswa_id</a>
+</div>
+<div class="mt-2 hover:underline"><a href="https://instagram.com/lokermahasiswa_jogja"
+        class="flex flex-row gap-2 items-center text-center" target="_blank">
+        <img src="/images/instagram.png" alt="">@lokermahasiswa_jogja</a>
+</div>
+<div class="mt-2 hover:underline"><a href="https://instagram.com/lokermahasiswa_medan"
+        class="flex flex-row gap-2 items-center text-center" target="_blank">
+        <img src="/images/instagram.png" alt="">@lokermahasiswa_medan</a>
+</div>
+<div class="mt-2 hover:underline"><a href="https://instagram.com/lokermahasiswa_smg"
+        class="flex flex-row gap-2 items-center text-center" target="_blank">
+        <img src="/images/instagram.png" alt="">@lokermahasiswa_smg</a>
+</div>
+<div class="mt-2 hover:underline"><a href="https://instagram.com/lokermahasiswa_sby"
+        class="flex flex-row gap-2 items-center text-center" target="_blank">
+        <img src="/images/instagram.png" alt="">@lokermahasiswa_sby</a>
+</div>
+<div class="mt-2 hover:underline"><a href="https://instagram.com/lokermahasiswa_mlg"
+        class="flex flex-row gap-2 items-center text-center" target="_blank">
+        <img src="/images/instagram.png" alt="">@lokermahasiswa_mlg</a>
+</div>
+<div class="mt-2 hover:underline"><a href="https://instagram.com/lokermahasiswa_bdg"
+        class="flex flex-row gap-2 items-center text-center" target="_blank">
+        <img src="/images/instagram.png" alt="">@lokermahasiswa_bdg</a>
+</div>
+<div class="mt-2 hover:underline"><a href="https://instagram.com/lokermahasiswa_jakarta"
+        class="flex flex-row gap-2 items-center text-center" target="_blank">
+        <img src="/images/instagram.png" alt="">@lokermahasiswa_jakarta</a>
+</div>
+<div class="mt-2 hover:underline"><a href="https://instagram.com/lokermahasiswa_bali"
+        class="flex flex-row gap-2 items-center text-center" target="_blank">
+        <img src="/images/instagram.png" alt="">@lokermahasiswa_bali</a>
+</div>
